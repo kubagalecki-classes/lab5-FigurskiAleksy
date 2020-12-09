5d1a69ed09627cf531c2d546083fcb9c72b59563
FigurskiAleksy <aleksy.figurski@gmail.com>
2020-12-09 19:35:04 +0000

    1

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -8,5 +8,18 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+        for (Human & i : people) 
+        {
+        i.birthday();
+        }
+    std::vector<char> monst
+    for (auto it = people.rbegin(); it != people.rend(); it++) 
+    {
+        if(it->ismonstter())
+            monst.push_back('n')
+        else
+            monst.push_back('y');
+    }
+
+    return monst;
 }
